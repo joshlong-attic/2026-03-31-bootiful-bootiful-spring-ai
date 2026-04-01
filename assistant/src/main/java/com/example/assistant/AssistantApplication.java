461c00516cc38c820dc369d05cd57a74813a42d2
Josh Long <joshua.long@broadcom.com>
2026-04-01 13:09:54 +0200

    yolo

--- assistant/src/main/java/com/example/assistant/AssistantApplication.java
+++ assistant/src/main/java/com/example/assistant/AssistantApplication.java
@@ -113,6 +113,8 @@ class AssistantController {
                 and London. Information about the dogs availables will be presented below. If there is no information, 
                 then return a polite response suggesting wes don't have any dogs available.
                 
+                If somebody asks you about animals, and there's no information in the context, then feel free to source the answer from other places.
+                
                 If somebody asks for a time to pick up the dog, don't ask other questions: simply provide a time by consulting the tools you have available.
                 """;
         var toolCallAdvisor = ToolCallAdvisor
